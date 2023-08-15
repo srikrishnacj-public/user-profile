0ccd18a0f0bf5deba9f6bb950a561a786b8a7bfe
Cj <srikrishna.cj@gmail.com>
2023-08-15 16:14:33 +0530

    test

--- cloud-init/first-boot-setup.sh
+++ cloud-init/first-boot-setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 export SCRIPT_USER=cj;
+SCRIPT_ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 bash $SCRIPT_ROOT_DIR/scripts/create-user.sh
 bash $SCRIPT_ROOT_DIR/scripts/install-essentials.sh
 bash $SCRIPT_ROOT_DIR/scripts/ssh-config.sh
 
-sudo -u $SCRIPT_USER bash -c "bash $SCRIPT_ROOT_DIR/scripts/install-profile.sh"
+# sudo -u $SCRIPT_USER bash -c "bash $SCRIPT_ROOT_DIR/scripts/install-profile.sh"
