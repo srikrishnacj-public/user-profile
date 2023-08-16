c0ff9371c5cbc69ae096d21d2199b940300d3f95
Cj <srikrishna.cj@gmail.com>
2023-08-16 15:20:36 +0530

    tes

--- install/common-install.sh
+++ install/common-install.sh
@@ -4,6 +4,6 @@
 find $HOME/mydrive/user-profile/config/common -type f -name install.sh -exec bash {} \;
 
 echo '
-for f in $HOME/mydrive/user-profile/config//common/source.sh; do source $f; done
+for f in $(find $HOME/mydrive/user-profile/config -type f -name source.sh); do source $f; done
 source $HOME/mydrive/user-profile/profile/common/common-profile.sh
 ' >> "$HOME/.bashrc"
