a583c7b03bf994fed05a2b21f7cc0d34302b0cfc
Cj <srikrishna.cj@gmail.com>
2023-08-16 14:32:57 +0530

    test

--- install/common-install.sh
+++ install/common-install.sh
@@ -2,6 +2,8 @@
 
 echo "This is from common install" >> /tmp/run.log
 
+$HOME/mydrive/user-profile/config
+
 echo '
     source $HOME/mydrive/user-profile/profile/common/common-profile.sh
     ' >> "$HOME/.bashrc"
