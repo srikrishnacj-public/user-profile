bf0cf7772b2b629c651c22a2cff6a160914f038c
Cj <srikrishna.cj@gmail.com>
2023-08-16 15:18:46 +0530

    test

--- install/common-install.sh
+++ install/common-install.sh
@@ -4,10 +4,6 @@
 find $HOME/mydrive/user-profile/config/common -type f -name install.sh -exec bash {} \;
 
 echo '
-find $HOME/mydrive/user-profile/config -type f -name source.sh | while read line; do
-    echo $line
-    source $line
-    eval "$(bash $line)"
-done
+for f in $HOME/mydrive/user-profile/config//common/source.sh; do source $f; done
 source $HOME/mydrive/user-profile/profile/common/common-profile.sh
 ' >> "$HOME/.bashrc"
