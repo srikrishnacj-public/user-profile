ec5845e41e1b29f8dd59754a73133a71213f560a
Cj <srikrishna.cj@gmail.com>
2023-08-16 14:41:16 +0530

    test

--- profile/common/common-profile.sh
+++ profile/common/common-profile.sh
@@ -2,4 +2,5 @@
 
 eval "$(starship init bash)"
 
+find $HOME/mydrive/user-profile/config -type f -name source.sh -exec echo {} \;
 find $HOME/mydrive/user-profile/config -type f -name source.sh -exec source {} \;
