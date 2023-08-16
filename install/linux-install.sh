c12377559dd547b87823041e836d476d730bd72f
Cj <srikrishna.cj@gmail.com>
2023-08-16 13:47:11 +0530

    test

--- install/linux-install.sh
+++ install/linux-install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo "This is from linux install" >> /tmp/run.log
-
 echo '
     source $HOME/mydrive/user-profile/profile/linux/linux-profile.sh
     ' >> "$HOME/.bashrc"
--- profile/linux/linux-profile.sh
+++ profile/linux/linux-profile.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-echo "This is from linux profile"
 
-# cloudinit=$(cloud-init status)
-# if [[ "$cloudinit" == *"running"* ]]
-# then
-#   echo "Waiting for cloud init to finish"
-#   cloud-init status --wait
-# else
-# fi
+cloudinit=$(cloud-init status)
+if [[ "$cloudinit" == *"running"* ]]
+then
+  echo "Waiting for cloud init to finish"
+  cloud-init status --wait
+else
+fi
