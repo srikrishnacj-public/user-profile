a8d379080c73fa02924bf4e1e971081c52e9ec32
Cj <srikrishna.cj@gmail.com>
2023-08-16 14:53:44 +0530

    test

--- config/dircolors/source.sh
+++ config/dircolors/source.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
 eval $(dircolors ~/.dir_colors)
-# d=$HOME/mydrive/user-profile/config/dircolors/.dircolors
-# test -r $d && eval "$(dircolors $d)"
---
+++ config/hyperjs/install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rm ~/.hyper.js
+ln $HOME/mydrive/user-profile/config/hyperjs/.hyper.js $HOME/.hyper.js
