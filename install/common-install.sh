f58e4ecca7ffabe953bc4d5a7e26ea3ccabc6f9d
Cj <srikrishna.cj@gmail.com>
2023-08-16 15:14:14 +0530

    test

--- install/common-install.sh
+++ install/common-install.sh
@@ -5,6 +5,7 @@ find $HOME/mydrive/user-profile/config/common -type f -name install.sh -exec bas
 
 echo '
 find $HOME/mydrive/user-profile/config -type f -name source.sh | while read line; do
+    echo $line
     source $line
 done
 source $HOME/mydrive/user-profile/profile/common/common-profile.sh
