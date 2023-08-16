c695bcd9989c9cf88537699dfafadf243360280a
Cj <srikrishna.cj@gmail.com>
2023-08-16 15:13:33 +0530

    test

--- config/common/hyperjs/install.sh
+++ config/common/hyperjs/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 rm -f ~/.hyper.js
-ln $HOME/mydrive/user-profile/config/hyperjs/.hyper.js $HOME/.hyper.js
+ln $HOME/mydrive/user-profile/config/common/hyperjs/.hyper.js $HOME/.hyper.js
--- install/common-install.sh
+++ install/common-install.sh
@@ -4,11 +4,8 @@
 find $HOME/mydrive/user-profile/config/common -type f -name install.sh -exec bash {} \;
 
 echo '
-
 find $HOME/mydrive/user-profile/config -type f -name source.sh | while read line; do
     source $line
 done
-
 source $HOME/mydrive/user-profile/profile/common/common-profile.sh
-
-    ' >> "$HOME/.bashrc"
+' >> "$HOME/.bashrc"
