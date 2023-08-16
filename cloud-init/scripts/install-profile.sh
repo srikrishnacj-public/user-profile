38942f85995d8db87809b7b670c1bee2e251583e
Cj <srikrishna.cj@gmail.com>
2023-08-16 12:03:19 +0530

    test

--- cloud-init/scripts/install-profile.sh
+++ cloud-init/scripts/install-profile.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-git clone https://github.com/srikrishnacj-public/user-profile ~/mydrive/user-profile
+git clone https://github.com/srikrishnacj-public/user-profile /home/$SCRIPT_USER/mydrive/user-profile
+chown -R $SCRIPT_USER /home/$SCRIPT_USER
+chown -R $SCRIPT_USER /home/$SCRIPT_USER/mydrive/user-profile
 sudo -u $SCRIPT_USER bash -c "bash /home/$SCRIPT_USER/mydrive/user-profile/install/install.sh"
